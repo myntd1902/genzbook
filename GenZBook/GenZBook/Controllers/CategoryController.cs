3cd1a4a9b7d5032449f2b19ee98968ed523ad624
myntd1902 <myntd1902@gmail.com>
2022-09-20 14:30:41 +0700

    first commit

---
+++ GenZBook/GenZBook/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using GenZBook.Data;
+using GenZBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenZBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+                _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+                ModelState.AddModelError("Name", "Thứ tự không thể trùng với tên");
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index"); 
+            }
+            return View(obj);
+        }
+        
+    }
+}
---
+++ GenZBook/GenZBook/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace GenZBook.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Tên sản phẩm")]
+        public string Name { get; set; }
+        [DisplayName("Thứ tự")]
+        [Range(1,100,ErrorMessage="Vui lòng chỉ nhập số từ 1 đến 100")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDatetime { get; set; } = DateTime.Now;
+    }
+}
